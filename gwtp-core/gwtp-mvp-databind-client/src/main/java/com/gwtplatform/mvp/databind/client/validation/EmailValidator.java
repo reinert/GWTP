87dbcc421b3786698ed941735259963be843b093
Danilo Reinert <daniloreinert@gmail.com>
2014-01-14 17:32:32 -0300

    Javadocs for EmailValidator.

--- gwtp-core/gwtp-mvp-databind-client/src/main/java/com/gwtplatform/mvp/databind/client/validation/EmailValidator.java
+++ gwtp-core/gwtp-mvp-databind-client/src/main/java/com/gwtplatform/mvp/databind/client/validation/EmailValidator.java
@@ -3,6 +3,9 @@ package com.gwtplatform.mvp.databind.client.validation;
 import com.google.gwt.regexp.shared.RegExp;
 
 /**
+ * Validator for textual values that should follow the email pattern.
+ * Null values are optionally invalidated.
+ *
  * @author Danilo Reinert
  */
 public class EmailValidator<T> implements Validator<T, String> {
@@ -13,14 +16,37 @@ public class EmailValidator<T> implements Validator<T, String> {
     private final Validation nullValidation;
     private final boolean required;
 
+    /**
+     * Creates a {@link EmailValidator} with given message for invalid results.
+     *
+     * This validator will successfully validate null values.
+     *
+     * @param message invalid message
+     */
     public EmailValidator(String message) {
         this(message, false);
     }
 
+    /**
+     * Creates a {@link EmailValidator} with given message for invalid results.
+     *
+     * Null (and empty) values will be invalidated as well.
+     *
+     * @param message invalid message
+     */
     public EmailValidator(String message, boolean required) {
         this(message, required, null);
     }
 
+    /**
+     * Creates a {@link EmailValidator} with messages for invalid email values and null values.
+     * When a validation with a *null* or *empty* string occurs then an invalid {@link Validation} is returned
+     * with the #nullMessage inside. Differently, if a non-empty string is given, but don't satisfy the requirements,
+     * then an invalid {@link Validation} with the #invalidMessage is returned.
+     *
+     * @param invalidMessage invalid email message
+     * @param nullMessage null value message
+     */
     public EmailValidator(String invalidMessage, boolean required, String nullMessage) {
         this.required = required;
         this.invalidValidation = Validation.invalid(new ValidationMessage(invalidMessage, ValidationMessage.Type.ERROR));
