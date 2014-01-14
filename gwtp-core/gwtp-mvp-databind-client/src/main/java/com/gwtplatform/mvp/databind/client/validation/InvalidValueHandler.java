8c46f60dda06382d1234233c81bfdd70251dd163
Danilo Reinert <daniloreinert@gmail.com>
2014-01-14 17:08:08 -0300

    Javadocs for ValidationHandler interfaces.

--- gwtp-core/gwtp-mvp-databind-client/src/main/java/com/gwtplatform/mvp/databind/client/validation/InvalidValueHandler.java
+++ gwtp-core/gwtp-mvp-databind-client/src/main/java/com/gwtplatform/mvp/databind/client/validation/InvalidValueHandler.java
@@ -1,9 +1,18 @@
 package com.gwtplatform.mvp.databind.client.validation;
 
 /**
+ * Handler for invalid validation events.
+ *
  * @author Danilo Reinert
  */
 public interface InvalidValueHandler<T, F> {
 
+    /**
+     * Called when a *invalid* validation event occurs.
+     *
+     * @param object model
+     * @param value value that was flushed
+     * @param message validation message for user feedback
+     */
     void onInvalidValue(T object, F value, ValidationMessage message);
 }
--- gwtp-core/gwtp-mvp-databind-client/src/main/java/com/gwtplatform/mvp/databind/client/validation/ValidationHandler.java
+++ gwtp-core/gwtp-mvp-databind-client/src/main/java/com/gwtplatform/mvp/databind/client/validation/ValidationHandler.java
@@ -1,9 +1,20 @@
 package com.gwtplatform.mvp.databind.client.validation;
 
 /**
+ * This interface provides callbacks to handle validation events.
+ *
+ * There are two possible validation events: valid and invalid.
+ *
  * @author Danilo Reinert
  */
 public interface ValidationHandler<T, F> extends InvalidValueHandler<T, F> {
 
+    /**
+     * Called when a *valid* validation event occurs.
+     *
+     * @param object model
+     * @param value flushed value
+     * @param message validation message provided by the binding
+     */
     void onValidValue(T object, F value, ValidationMessage message);
 }
