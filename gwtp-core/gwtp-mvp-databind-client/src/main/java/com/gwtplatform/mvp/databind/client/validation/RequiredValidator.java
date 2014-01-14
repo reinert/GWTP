96a5d949bbdb556063dd351cd3171c7fe2d0c980
Danilo Reinert <daniloreinert@gmail.com>
2014-01-14 17:32:18 -0300

    Javadocs for RequiredValidator.

--- gwtp-core/gwtp-mvp-databind-client/src/main/java/com/gwtplatform/mvp/databind/client/validation/RequiredValidator.java
+++ gwtp-core/gwtp-mvp-databind-client/src/main/java/com/gwtplatform/mvp/databind/client/validation/RequiredValidator.java
@@ -1,12 +1,21 @@
 package com.gwtplatform.mvp.databind.client.validation;
 
 /**
+ * Validator for non-null properties.
+ *
+ * Empty string are treated as invalid too.
+ *
  * @author Danilo Reinert
  */
 public class RequiredValidator<T, F> implements Validator<T, F> {
 
     private final Validation invalid;
 
+    /**
+     * Creates a {@link RequiredValidator} with given message for invalid results.
+     *
+     * @param message invalid message
+     */
     public RequiredValidator(String message) {
         this.invalid = Validation.invalid(new ValidationMessage(message, ValidationMessage.Type.ERROR));
     }
