41bbdf6084b8307b511b31f0c49f5a8f41d94882
Danilo Reinert <daniloreinert@gmail.com>
2014-01-14 17:45:42 -0300

    Javadocs for Formatter.

--- gwtp-core/gwtp-mvp-databind-client/src/main/java/com/gwtplatform/mvp/databind/client/format/Formatter.java
+++ gwtp-core/gwtp-mvp-databind-client/src/main/java/com/gwtplatform/mvp/databind/client/format/Formatter.java
@@ -3,10 +3,19 @@ package com.gwtplatform.mvp.databind.client.format;
 import javax.annotation.Nullable;
 
 /**
+ * Two-way formatter used for formatting and parsing values to/from view.
+ *
  * @author Danilo Reinert
  */
 public interface Formatter<MODEL, VIEW> extends ReadFormatter<MODEL, VIEW> {
 
+    /**
+     * Given the formatted value, it parses into a row value for sending to the model.
+     * The Model should know only about the raw value.
+     *
+     * @param formattedValue held by the view
+     * @return raw value
+     */
     @Nullable
-    MODEL unformat(@Nullable VIEW value);
+    MODEL unformat(@Nullable VIEW formattedValue);
 }
