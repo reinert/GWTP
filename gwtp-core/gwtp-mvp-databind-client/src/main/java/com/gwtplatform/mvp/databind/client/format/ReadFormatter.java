8fb0b7192ad576c0087ec12098a4c11f4d8c5ba2
Danilo Reinert <daniloreinert@gmail.com>
2014-01-14 17:45:28 -0300

    Javadocs for ReadFormatter.

--- gwtp-core/gwtp-mvp-databind-client/src/main/java/com/gwtplatform/mvp/databind/client/format/ReadFormatter.java
+++ gwtp-core/gwtp-mvp-databind-client/src/main/java/com/gwtplatform/mvp/databind/client/format/ReadFormatter.java
@@ -3,10 +3,20 @@ package com.gwtplatform.mvp.databind.client.format;
 import javax.annotation.Nullable;
 
 /**
+ * One-way formatter for read-only circumstances.
+ * Usually it will format a raw value to nicely present to the user.
+ *
  * @author Danilo Reinert
  */
 public interface ReadFormatter<MODEL, VIEW> {
 
+    /**
+     * Given the raw value, it returns a formatted value for view presentation.
+     * The View should know only about the formatted value.
+     *
+     * @param rawValue raw value held by the model
+     * @return formatted value
+     */
     @Nullable
-    VIEW format(@Nullable MODEL value);
+    VIEW format(@Nullable MODEL rawValue);
 }
