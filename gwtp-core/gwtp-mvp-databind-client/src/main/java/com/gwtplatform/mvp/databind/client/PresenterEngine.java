c1fe239accb1b408490c0cd80d12ce1aa5288d42
Danilo Reinert <daniloreinert@gmail.com>
2014-01-14 03:29:54 -0300

    PresenterEngine.

---
+++ gwtp-core/gwtp-mvp-databind-client/src/main/java/com/gwtplatform/mvp/databind/client/PresenterEngine.java
@@ -0,0 +1,218 @@
+package com.gwtplatform.mvp.databind.client;
+
+
+import com.google.web.bindery.event.shared.HandlerRegistration;
+import com.gwtplatform.mvp.databind.client.format.Formatter;
+import com.gwtplatform.mvp.databind.client.format.ReadFormatter;
+import com.gwtplatform.mvp.databind.client.property.PropertyAccessor;
+import com.gwtplatform.mvp.databind.client.property.ProvidesValue;
+import com.gwtplatform.mvp.databind.client.validation.Validation;
+import com.gwtplatform.mvp.databind.client.validation.Validator;
+
+import javax.annotation.Nullable;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * @author Danilo Reinert
+ */
+@SuppressWarnings("unchecked")
+public class PresenterEngine<T> implements PropertyBinder<T>, Iterable<String> {
+
+    private static class Holder {
+
+        PropertyAccessor propertyAccessor;
+        Validator validatesValue;
+        Formatter formatter;
+        boolean autoBind;
+
+        Holder(boolean autoBind, PropertyAccessor propertyAccessor, Validator validatesValue, Formatter formatter) {
+            this.propertyAccessor = propertyAccessor;
+            this.validatesValue = validatesValue;
+            this.formatter = formatter;
+            this.autoBind = autoBind;
+        }
+
+        Holder(boolean autoBind, PropertyAccessor propertyAccessor) {
+            this(autoBind, propertyAccessor, null, null);
+        }
+
+        Holder(boolean autoBind, PropertyAccessor propertyAccessor, Formatter formatter) {
+            this(autoBind, propertyAccessor, null, formatter);
+        }
+
+        Holder(boolean autoBind, PropertyAccessor propertyAccessor, Validator validatesValue) {
+            this(autoBind, propertyAccessor, validatesValue, null);
+        }
+    }
+
+    private final Map<String, Holder> holderMap = new LinkedHashMap<String, Holder>();
+
+    public <F> HandlerRegistration bindProperty(String id, PropertyAccessor<T, F> propertyAccessor) {
+        return bindProperty(id, propertyAccessor, null, null);
+    }
+
+    public <F> HandlerRegistration bindProperty(boolean autoBind, String id, PropertyAccessor<T, F> propertyAccessor) {
+        return bindProperty(autoBind, id, propertyAccessor, null, null);
+    }
+
+    public <F> HandlerRegistration bindProperty(boolean autoBind, String id, final ProvidesValue<T, F> providesValue) {
+        return bindProperty(autoBind, id, new PropertyAccessor<T, F>() {
+            @Override
+            public void setValue(T object, F value) {
+            }
+
+            @Override
+            public F getValue(T object) {
+                return providesValue.getValue(object);
+            }
+        }, null, null);
+    }
+
+    public <F> HandlerRegistration bindProperty(String id, final ProvidesValue<T, F> providesValue) {
+        return bindProperty(id, providesValue, null);
+    }
+
+    @Override
+    public <F> HandlerRegistration bindProperty(String id, ProvidesValue<T, F> providesValue, ReadFormatter<F, ?> readFormatter) {
+        return bindProperty(true, id, providesValue, readFormatter);
+    }
+
+    @Override
+    public <F> HandlerRegistration bindProperty(String id, PropertyAccessor<T, F> propertyAccessor, Formatter<F, ?> formatter) {
+        return bindProperty(true, id, propertyAccessor, formatter);
+    }
+
+    public <F> HandlerRegistration bindProperty(String id, PropertyAccessor<T, F> propertyAccessor, Validator<T, F> validatesValue) {
+        return bindProperty(true, id, propertyAccessor, validatesValue, null);
+    }
+
+    @Override
+    public <F> HandlerRegistration bindProperty(String id, PropertyAccessor<T, F> propertyAccessor, Validator<T, F> validatesValue,
+                                 Formatter<F, ?> formatter) {
+        return bindProperty(true, id, propertyAccessor, validatesValue, formatter);
+    }
+
+    @Override
+    public <F> HandlerRegistration bindProperty(boolean autoBind, String id, final ProvidesValue<T, F> providesValue,
+                                 ReadFormatter<F, ?> readFormatter) {
+        return bindProperty(id, new PropertyAccessor<T, F>() {
+            @Override
+            public void setValue(T object, F value) {
+            }
+
+            @Override
+            public F getValue(T object) {
+                return providesValue.getValue(object);
+            }
+        }, readFormatter);
+    }
+
+    @Override
+    public <F> HandlerRegistration bindProperty(boolean autoBind, String id, PropertyAccessor<T, F> propertyAccessor,
+                                 Formatter<F, ?> formatter) {
+        return bindProperty(autoBind, id, propertyAccessor, null, null);
+    }
+
+    @Override
+    public <F> HandlerRegistration bindProperty(boolean autoBind, String id, PropertyAccessor<T, F> propertyAccessor,
+                                 Validator<T, F> validatesValue) {
+        return bindProperty(autoBind, id, propertyAccessor, validatesValue, null);
+    }
+
+    @Override
+    public <F> HandlerRegistration bindProperty(boolean autoBind, String id, PropertyAccessor<T, F> propertyAccessor,
+                                 Validator<T, F> validatesValue, Formatter<F, ?> formatter) {
+        if (holderMap.containsKey(id)) {
+            Holder tHolder = holderMap.get(id);
+            tHolder.propertyAccessor = propertyAccessor;
+            tHolder.validatesValue = validatesValue;
+            tHolder.formatter = formatter;
+        } else {
+            Holder tHolder = new Holder(autoBind, propertyAccessor, validatesValue, formatter);
+            holderMap.put(id, tHolder);
+        }
+        return BinderHandlerRegistration.of(this, id);
+    }
+
+    public Validation isValueValid(String id, @Nullable T data, Object value) {
+        if (data != null) {
+            Holder holder = holderMap.get(id);
+            if (holder != null && holder.validatesValue != null) return holder.validatesValue.validate(data, value);
+        }
+        return Validation.valid();
+    }
+
+    public Object getValue(String id, @Nullable T data) {
+        if (data != null) {
+            Holder holder = holderMap.get(id);
+            if (holder != null) return holder.propertyAccessor.getValue(data);
+        }
+        return null;
+    }
+
+    public Object getFormattedValue(String id, @Nullable T data) {
+        if (data != null) {
+            Holder holder = holderMap.get(id);
+            if (holder != null) {
+                if (holder.formatter != null) {
+                    return holder.formatter.format(holder.propertyAccessor.getValue(data));
+                }
+                return holder.propertyAccessor.getValue(data);
+            }
+        }
+        return null;
+    }
+
+    public void setValue(String id, @Nullable T data, Object value) {
+        if (data != null) {
+            Holder holder = holderMap.get(id);
+            if (holder != null) holder.propertyAccessor.setValue(data, value);
+        }
+    }
+
+    public void setFormattedValue(String id, @Nullable T data, Object value) {
+        if (data != null) {
+            Holder holder = holderMap.get(id);
+            if (holder != null) {
+                if (holder.formatter != null) {
+                    holder.propertyAccessor.setValue(data, holder.formatter.unformat(value));
+                } else {
+                    holder.propertyAccessor.setValue(data, value);
+                }
+            }
+        }
+    }
+
+    public PropertyAccessor<T, ?> getPropertyAccessor(String id) {
+        return holderMap.get(id).propertyAccessor;
+    }
+
+    public Validator<T, ?> getValidatesValue(String id) {
+        return holderMap.get(id).validatesValue;
+    }
+
+    public boolean isAutoBind(String id) {
+        return holderMap.get(id).autoBind;
+    }
+
+    public boolean hasProperty(String id) {
+        return holderMap.containsKey(id);
+    }
+
+    /**
+     * Returns an iterator over a set of elements of type T.
+     *
+     * @return an Iterator.
+     */
+    @Override
+    public Iterator<String> iterator() {
+        return holderMap.keySet().iterator();
+    }
+
+    @Override
+    public boolean unbind(String id) {
+        return holderMap.remove(id) != null;
+    }
+}
