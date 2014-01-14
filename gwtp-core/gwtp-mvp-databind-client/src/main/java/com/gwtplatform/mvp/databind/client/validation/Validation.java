ae41c9d0d519cd80c7cadfeb4c1bf76f7460533f
Danilo Reinert <daniloreinert@gmail.com>
2014-01-14 17:06:15 -0300

    Javadocs for Validation.

--- gwtp-core/gwtp-mvp-databind-client/src/main/java/com/gwtplatform/mvp/databind/client/validation/Validation.java
+++ gwtp-core/gwtp-mvp-databind-client/src/main/java/com/gwtplatform/mvp/databind/client/validation/Validation.java
@@ -3,12 +3,16 @@ package com.gwtplatform.mvp.databind.client.validation;
 import javax.annotation.Nullable;
 
 /**
- * Created at 09/09/13 02:04
+ * Object used as return of validations by {@link Validator}.
+ *
+ * It provides a simple boolean result stating whether the validation was successful or not
+ * and an optional {@link ValidationMessage} with some useful feedback for the user.
  *
  * @author Danilo Reinert
  */
 public class Validation {
 
+    // Cache recurring results
     protected static Validation defaultInvalid = new Validation(false, null);
     protected static Validation defaultValid = new Validation(true, null);
 
@@ -31,15 +35,36 @@ public class Validation {
     private final boolean valid;
     private final ValidationMessage validationMessage;
 
+    /**
+     * This constructor should not be externally used.
+     * Otherwise, it is recommended that static factory methods be made available for instantiation.
+     *
+     * It is left as protected in order to allow inheritance.
+     *
+     * @param valid validation result
+     * @param validationMessage validation message for the user
+     */
     protected Validation(boolean valid, @Nullable ValidationMessage validationMessage) {
         this.valid = valid;
         this.validationMessage = validationMessage;
     }
 
+    /**
+     * Valid is the final result of some validation.
+     * It simply tells if the validation was successful or not.
+     *
+     * @return {@code true} if valid, {@code false} otherwise.
+     */
     public boolean isValid() {
         return valid;
     }
 
+    /**
+     * Validation message is an optional feedback for the user.
+     * It can be just a neutral info or a useful guideline for handling some error.
+     *
+     * @return validation message
+     */
     @Nullable
     public ValidationMessage getValidationMessage() {
         return validationMessage;
