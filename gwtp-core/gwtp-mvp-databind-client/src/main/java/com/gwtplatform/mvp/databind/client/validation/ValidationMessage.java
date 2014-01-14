374f0cef63d3a9b28e6d3c95ccc1ba332897223e
Danilo Reinert <daniloreinert@gmail.com>
2014-01-14 17:06:35 -0300

    Javadocs for ValidationMessage.

--- gwtp-core/gwtp-mvp-databind-client/src/main/java/com/gwtplatform/mvp/databind/client/validation/ValidationMessage.java
+++ gwtp-core/gwtp-mvp-databind-client/src/main/java/com/gwtplatform/mvp/databind/client/validation/ValidationMessage.java
@@ -1,11 +1,22 @@
 package com.gwtplatform.mvp.databind.client.validation;
 
 /**
+ * Object that holds a textual message for the user and a type for styling purposes.
+ *
+ * Usually this message will provide a feedback notifying you him of some occurrence
+ * or enabling him to take some action in order to handle an error.
+ *
  * @author Danilo Reinert
  */
 public class ValidationMessage {
 
-    public static enum Type {SUCCESS, ERROR, WARNING, INFO, NONE}
+    /**
+     * The message type is useful for styling user message.
+     *
+     * Your {@link ValidationHandler} can present the message in different ways
+     * depending on the type.
+     */
+    public static enum Type {ERROR, INFO, NONE, SUCCESS, WARNING}
 
     private final String message;
     private final Type type;
@@ -15,10 +26,23 @@ public class ValidationMessage {
         this.type = type;
     }
 
+    /**
+     * Retrieves the user message.
+     *
+     * @return message for the user.
+     */
     public String getMessage() {
         return message;
     }
 
+    /**
+     * Retrieves the type of the Validation.
+     * It can be useful for decorating message's container in a enlightening way.
+     *
+     * @return validation type
+     *
+     * @see ValidationMessage.Type
+     */
     public Type getType() {
         return type;
     }
