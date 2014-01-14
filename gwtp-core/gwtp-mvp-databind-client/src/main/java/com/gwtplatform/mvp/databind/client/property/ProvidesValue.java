c1b88757102a4a7fdcb437528bd6cac06db6813f
Danilo Reinert <daniloreinert@gmail.com>
2014-01-14 19:37:58 -0300

    Javadocs for ProvidesValue.

--- gwtp-core/gwtp-mvp-databind-client/src/main/java/com/gwtplatform/mvp/databind/client/property/ProvidesValue.java
+++ gwtp-core/gwtp-mvp-databind-client/src/main/java/com/gwtplatform/mvp/databind/client/property/ProvidesValue.java
@@ -3,9 +3,18 @@ package com.gwtplatform.mvp.databind.client.property;
 import javax.annotation.Nullable;
 
 /**
+ * A value provider of an object, usually calling some getter.
+ *
  * @author Danilo Reinert
  */
 public interface ProvidesValue<T, F> {
 
+    /**
+     * Given a object, returns the value of a property.
+     * The property is known at compile time.
+     *
+     * @param t object to be accessed
+     * @return value of a property of the object
+     */
     public @Nullable F getValue(T t);
 }
