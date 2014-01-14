e7d7eca647d84b7e6f38e25a4588c7aec4c07455
Danilo Reinert <daniloreinert@gmail.com>
2014-01-14 19:38:14 -0300

    Javadocs for PropertyAccessor.

--- gwtp-core/gwtp-mvp-databind-client/src/main/java/com/gwtplatform/mvp/databind/client/property/PropertyAccessor.java
+++ gwtp-core/gwtp-mvp-databind-client/src/main/java/com/gwtplatform/mvp/databind/client/property/PropertyAccessor.java
@@ -3,9 +3,24 @@ package com.gwtplatform.mvp.databind.client.property;
 import javax.annotation.Nullable;
 
 /**
+ * This interface is meant to provide access to some property of an object.
+ * Simply, it should call the getter and setter of a specific property.
+ *
+ * Serves as support for third-party subjects to manage object properties,
+ * following the IoC pattern.
+ *
+ * @see <a href="http://martinfowler.com/bliki/InversionOfControl.html">InversionOfControl</a>
+ *
  * @author Danilo Reinert
  */
 public interface PropertyAccessor<T, F> extends ProvidesValue<T, F> {
 
+    /**
+     * Sets the given value to the object.
+     * The property is known at compile time.
+     *
+     * @param t the object containing the property
+     * @param value the value to be set into object's property
+     */
     void setValue(T t, @Nullable F value);
 }
