32e5e1e9e7d0d60da9b218eed76128148f3ecd77
Danilo Reinert <daniloreinert@gmail.com>
2014-01-14 21:25:15 -0300

    Binder Javadocs.

--- gwtp-core/gwtp-mvp-databind-client/src/main/java/com/gwtplatform/mvp/databind/client/Binder.java
+++ gwtp-core/gwtp-mvp-databind-client/src/main/java/com/gwtplatform/mvp/databind/client/Binder.java
@@ -1,9 +1,20 @@
 package com.gwtplatform.mvp.databind.client;
 
 /**
+ * It defines an object that supports binding.
+ *
+ * The underlying implementation should provide methods for binding instances to ids
+ * and must support an #unbind method that cancels the binding of some id.
+ *
  * @author Danilo Reinert
  */
 public interface Binder {
 
+    /**
+     * Releases any binding previously made with the given id.
+     *
+     * @param id id of the binding
+     * @return {@code true} if any binding associated with the id was removed, {@code false} otherwise.
+     */
     boolean unbind(String id);
 }
