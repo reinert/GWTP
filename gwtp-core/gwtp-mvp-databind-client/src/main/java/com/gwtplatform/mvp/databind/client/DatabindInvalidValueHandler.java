8d71baf0201c9ce861d5ea341a23478f80de749e
Danilo Reinert <daniloreinert@gmail.com>
2014-01-17 20:45:40 -0300

    DatabindValidationHandlers Javadocs.

--- gwtp-core/gwtp-mvp-databind-client/src/main/java/com/gwtplatform/mvp/databind/client/DatabindInvalidValueHandler.java
+++ gwtp-core/gwtp-mvp-databind-client/src/main/java/com/gwtplatform/mvp/databind/client/DatabindInvalidValueHandler.java
@@ -2,10 +2,26 @@ package com.gwtplatform.mvp.databind.client;
 
 import com.gwtplatform.mvp.databind.client.validation.ValidationMessage;
 
+import javax.annotation.Nullable;
+
 /**
+ * Handler for invalid validation events bound to properties.
+ *
+ * @see com.gwtplatform.mvp.databind.client.validation.InvalidValueHandler
+ *
  * @author Danilo Reinert
  */
 public interface DatabindInvalidValueHandler {
 
-    <T, F> void onInvalidValue(String property, T object, F value, ValidationMessage message);
+    /**
+     * Notify the view of a invalid try to update a property on the model.
+     *
+     * @param property model's property id
+     * @param object model object
+     * @param value updated value
+     * @param message message from presenter
+     * @param <T>
+     * @param <F>
+     */
+    <T, F> void onInvalidValue(String property, T object, F value, @Nullable ValidationMessage message);
 }
--- gwtp-core/gwtp-mvp-databind-client/src/main/java/com/gwtplatform/mvp/databind/client/DatabindValidationHandler.java
+++ gwtp-core/gwtp-mvp-databind-client/src/main/java/com/gwtplatform/mvp/databind/client/DatabindValidationHandler.java
@@ -2,10 +2,26 @@ package com.gwtplatform.mvp.databind.client;
 
 import com.gwtplatform.mvp.databind.client.validation.ValidationMessage;
 
+import javax.annotation.Nullable;
+
 /**
+ * This interface provides callbacks to handle validation events bound to properties.
+ *
+ * @see com.gwtplatform.mvp.databind.client.validation.ValidationHandler
+ *
  * @author Danilo Reinert
  */
 public interface DatabindValidationHandler extends DatabindInvalidValueHandler {
 
-    <T, F> void onValidValue(String property, T object, F value, ValidationMessage message);
+    /**
+     * Notify the view of a property updated on model.
+     *
+     * @param property model's property id
+     * @param object model object
+     * @param value updated value
+     * @param message message from presenter
+     * @param <T>
+     * @param <F>
+     */
+    <T, F> void onValidValue(String property, T object, F value, @Nullable ValidationMessage message);
 }
