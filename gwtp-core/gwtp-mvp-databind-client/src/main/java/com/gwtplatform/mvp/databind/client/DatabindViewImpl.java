ccee89827dc43f3627add38ba31128c752a8da96
Danilo Reinert <daniloreinert@gmail.com>
2014-01-17 21:50:41 -0300

    Added all-ready DatabindView implementation.

---
+++ gwtp-core/gwtp-mvp-databind-client/src/main/java/com/gwtplatform/mvp/databind/client/DatabindViewImpl.java
@@ -0,0 +1,54 @@
+package com.gwtplatform.mvp.databind.client;
+
+import com.google.gwt.user.client.TakesValue;
+import com.google.gwt.user.client.ui.HasValue;
+import com.google.web.bindery.event.shared.HandlerRegistration;
+import com.gwtplatform.mvp.client.ViewWithUiHandlers;
+import com.gwtplatform.mvp.databind.client.validation.InvalidValueHandler;
+import com.gwtplatform.mvp.databind.client.validation.ValidationHandler;
+import com.gwtplatform.mvp.databind.client.validation.ValidationMessage;
+
+/**
+ * @author Danilo Reinert
+ */
+//TODO: rearrangement and docs
+public class DatabindViewImpl<H extends DatabindUiHandlers> extends ViewWithUiHandlers<H>
+        implements DatabindView<H> {
+    
+    private final DatabindViewEngine engine = new DatabindViewEngine();
+
+    @Override
+    public boolean unbind(String id) {return engine.unbind(id);}
+
+    @Override
+    public <F> HandlerRegistration bindReadOnlyWidget(String id, TakesValue<F> widget) {return engine.bindReadOnlyWidget(id, widget);}
+
+    @Override
+    public <F> HandlerRegistration bindWidget(String id, HasValue<F> widget) {return engine.bindWidget(id, widget);}
+
+    @Override
+    public void setUiHandlers(H uiHandlers) {
+        super.setUiHandlers(uiHandlers);
+        engine.setUiHandlers(uiHandlers);
+    }
+
+    @Override
+    public <F> void setValue(String id, F value) {engine.setValue(id, value);}
+
+    @Override
+    public <F> F getValue(String id) {return engine.getValue(id);}
+
+    @Override
+    public <T, F> HandlerRegistration bindValidationHandler(String id, InvalidValueHandler<T, F> invalidValueHandler) {return engine.bindValidationHandler(id, invalidValueHandler);}
+
+    @Override
+    public <T, F> HandlerRegistration bindValidationHandler(String id, ValidationHandler<T, F> validationHandler) 
+    {return engine.bindValidationHandler(id, validationHandler);}
+
+    @Override
+    public <T, F> void onValidValue(String id, T object, F value, ValidationMessage message) {engine.onValidValue(id,
+            object, value, message);}
+
+    @Override
+    public <T, F> void onInvalidValue(String id, T object, F value, ValidationMessage message) {engine.onInvalidValue(id, object, value, message);}
+}
